6a2f8b207f27978d99e4a6a38d9aa85f69108a4a
Josh Owens <me@jowens.dev>
2024-01-30 21:31:42 -0600

    colly

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	fmt.Println("Hello, Scraper!")
+	c := colly.NewCollector(
+		colly.AllowedDomains("broadwayinchicago.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://broadwayinchicago.com/chicago-broadway-tickets/special-offer-coupon-codes/")
 }
